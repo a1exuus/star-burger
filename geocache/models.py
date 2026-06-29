ffb7e7f02fccfacef18ae203ea4b77bbf4103bd2
alexuus <alexexmypride@gmail.com>
2026-06-29 11:51:40 +0300

    allowed some fields to be empty

--- geocache/models.py
+++ geocache/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Location(models.Model):
     address = models.CharField('Адрес', max_length=255, unique=True)
-    latitude = models.FloatField('Широта')
-    longitude = models.FloatField('Долгота')
+    latitude = models.FloatField('Широта', null=True)
+    longitude = models.FloatField('Долгота', null=True)
     
     updated_at = models.DateTimeField('Дата последнего запроса', auto_now=True)
 
